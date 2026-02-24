1e2640ce8df8d9abe743c23fece0e9adff4784a8
HY Tan <hsuehyungtan@gmail.com>
2026-02-24 19:51:34 +0800

    fix: fix nextConfig assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'export',
   basePath: '/v2x-protocol-toolkit',
-  assetPrefix: '/v2x-protocol-toolkit',
+  assetPrefix: '/v2x-protocol-toolkit/',
   images: {
     unoptimized: true,
   },
